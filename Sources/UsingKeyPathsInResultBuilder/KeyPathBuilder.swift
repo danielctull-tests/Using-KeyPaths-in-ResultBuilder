111a861eb39d4f4f9356d2742d0b6189b88ca749
Daniel Tull <dt@danieltull.co.uk>
2024-02-19 15:12:47 +0000

    Fix the issue by adding a buildExpression for keypath

--- Sources/UsingKeyPathsInResultBuilder/KeyPathBuilder.swift
+++ Sources/UsingKeyPathsInResultBuilder/KeyPathBuilder.swift
@@ -2,6 +2,12 @@
 @resultBuilder
 public enum KeyPathBuilder<Root> {
 
+    public static func buildExpression<A>(
+        _ expression: KeyPath<Root, A>
+    ) -> KeyPath<Root, A> {
+        expression
+    }
+
     public static func buildBlock<A>(
         _ keyPath: KeyPath<Root, A>
     ) -> KeyPath<Root, A> {
